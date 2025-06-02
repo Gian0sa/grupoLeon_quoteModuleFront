f66012c9fb9780cfe22f6f0ee3432c364310eb01
omarcm <omarcm070400@gmail.com>
2025-06-02 13:57:18 -0500

    [feat] prueba de ruta , deploy

--- src/app/Routes.jsx
+++ src/app/Routes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { Login } from "../features/auth/pages/Login";
 import { Register } from "../features/auth/pages/Register";
 import { ClientPage } from "../features/clients/pages/ClientPage";
 import { DashboardPage } from "../features/dashboard/pages/DashboardPage";
@@ -14,8 +13,6 @@ import { HistoryQuotesPage } from "../features/quotes/pages/HistoryQuotesPage";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-
 
       <Route path="/register" element={
         <PrivateRoute>
