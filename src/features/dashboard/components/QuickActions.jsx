feae1461999a92691e0edfd4eb24078a5f572964
omarcaballero <omarcm070400@gmail.com>
2025-09-15 11:36:54 -0500

    [feat] añadido boton de lista de precios

--- src/features/dashboard/components/QuickActions.jsx
+++ src/features/dashboard/components/QuickActions.jsx
@@ -1,5 +1,9 @@
 import { HStack, Button, Icon } from "@chakra-ui/react";
-import { MdBarChart, MdBubbleChart } from "react-icons/md";
+import { 
+  MdOutlineTrackChanges, 
+  MdOutlineAttachMoney, 
+  MdOutlinePriceCheck 
+} from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import styles from "./QuickActions.module.css"; 
 
@@ -7,20 +11,22 @@ export function QuickActions() {
   const navigate = useNavigate();
 
   const actions = [
-    { label: "Seguimiento de Pedidos", icon: MdBarChart, path: "/reports" },
-    { label: "Cuentas por Cobrar", icon: MdBubbleChart, path: "/receivable" },
+    { label: "Lista de Precios", icon: MdOutlinePriceCheck, path: "/productsPriceList" },
+    { label: "Mis Pedidos", icon: MdOutlineTrackChanges, path: "/reports" },
+    { label: "Cuentas por Cobrar", icon: MdOutlineAttachMoney, path: "/receivable" },
   ]; 
+
   return (
-    <HStack spacing={3} className={styles.btnsgroup}>
+    <HStack spacing={2} className={styles.btnsgroup}>
       {actions.map((action) => (
         <Button
           key={action.path}
           leftIcon={<Icon as={action.icon} />}
-          bg="whiteAlpha.200"
+          bg="whiteAlpha.300"
           color="white"
           size="md"
           borderRadius="full"
-          _hover={{ bg: "whiteAlpha.300" }}
+          _hover={{ bg: "whiteAlpha.400" }}
           className={styles.btnactionsdash}
           onClick={() => navigate(action.path)}
         >
